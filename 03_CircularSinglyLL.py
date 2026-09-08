f783cf2dfab00b61315eba667d4694554e5d4bfb
Akshay Das <akshay98322@github.com>
2026-09-08 18:45:28 +0530

    Refactor CircularSinglyLinkedList methods for improved clarity; update error messages and formatting

--- 03_CircularSinglyLL.py
+++ 03_CircularSinglyLL.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = None
 
+
 class CircularSinglyLinkedList:
     def __init__(self):
         self.head = None
@@ -33,11 +34,12 @@ class CircularSinglyLinkedList:
             current.next = new_node
         self.head = new_node
 
-    """ Insert after an element """
+    """Insert after an element"""
     def insert_after(self, target_data, data):
         if self.head is None:
             print("Linked List is empty")
             return
+
         current = self.head
         while True:
             if current.data == target_data:
@@ -48,20 +50,21 @@ class CircularSinglyLinkedList:
             current = current.next
             if current is self.head:
                 break
-        print("Target Data doesnt exist")
+        print("Element not found")
 
     """Print the Linked List"""
     def display(self):
         if self.head is None:
             print("Linked List is empty")
             return
+
         current = self.head
         while True:
             print(current.data, end=" -> ")
             current = current.next
             if current is self.head:
                 break
-        print("")
+        print("head")
 
     """Delete an element from the List"""
     def delete(self, data):
@@ -72,8 +75,9 @@ class CircularSinglyLinkedList:
 
         # Case2: If the length is one and item exits
         if self.head.data == data and self.head.next is self.head:
-            self.head = Node
+            self.head = None
             return
+
         current = self.head
         previous = None
         #Case3: Delete the head where there are other Nodes
@@ -89,18 +93,21 @@ class CircularSinglyLinkedList:
             if current.data == data:
                 previous.next = current.next
                 return
+
             previous = current
             current = current.next
             if current is self.head:
                 break
+
         print("Element not found")
-           
+
     """Verify Circular linked list"""
     def check_circularity(self):
         ctr = 0
         if self.head is None:
             print("Linked List is empty")
             return
+
         current = self.head
         while True:
             print(current.data, end=" -> ")
@@ -108,7 +115,7 @@ class CircularSinglyLinkedList:
             if ctr >= 20:
                 break
             ctr += 1
-        print("")
+        print("head")
 
 
 ll = CircularSinglyLinkedList()
@@ -153,5 +160,6 @@ ll.delete(4)
 ll.display()
 print("Delete an element which doesn't exist")
 ll.delete(10)
+ll.display()
 
 ll.check_circularity()
