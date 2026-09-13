afce3ba57146dc75b0d7d56003dab05a67dc8854
Akshay Das <akshay98322@github.com>
2026-09-13 20:18:56 +0530

    Remove print statements for empty stack conditions in pop and peek methods

--- 05_Python_F_Stack.py
+++ 05_Python_F_Stack.py
@@ -13,14 +13,12 @@ class Stack:
 
     def pop(self):
         if self.is_empty():
-            print("*** The stack is empty")
             return
         else:
             return self.elements.pop()
 
     def peek(self):
         if self.is_empty():
-            print("*** The stack is empty")
             return
         else:
             return self.elements[-1]
